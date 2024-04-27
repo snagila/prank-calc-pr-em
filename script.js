a5e6ebcd926a992ca1eab8f2b668743c4f1f7c72
Sachin Nagila <sachinnagila2053@gmail.com>
2024-04-27 22:00:45 +1000

    numbers display and eval executed

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-// .arbutus-regular {
-//     font-family: "Arbutus", serif;
-//     font-weight: 400;
-//     font-style: normal;
-//   }
-  
-.body {
-background: black
-}
+const allButtonsElm = document.querySelectorAll(".btn");
+const displayElm = document.querySelector(".display");
+let strToDisplay = "";
+
+const buttonAction = (value) => {
+  if (value === "AC") {
+    strToDisplay = "";
+    return display(strToDisplay);
+  }
+  if (value === "=") {
+    const total = eval(strToDisplay);
+    display(total);
+    return;
+  }
+  strToDisplay += value;
+  display(strToDisplay);
+};
+
+// attached click event to all the buttons
+allButtonsElm.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.innerText;
+    buttonAction(value);
+  });
+});
+
+// update clicked button value to display area
+const display = (str) => {
+  displayElm.innerText = str || "0.0";
+};
+
+// calculate total
+// const displayTotal = () => {
+//   const total = eval(strToDisplay);
+// };
